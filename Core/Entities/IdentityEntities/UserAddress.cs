868b451d5d1f88c18b932fd79c18741c190648d8
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-02 18:35:47 +0300

    Add AppUser class that inherit from IdentityUser to add Name property and UserAddress class

---
+++ Core/Entities/IdentityEntities/UserAddress.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities.IdentityEntities
+{
+	public class UserAddress: BaseEntity
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Street { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+		public string AppUserId { get; set; } // FK For User Table | and we don't need to write it in fluent api because his name => 'tableName+Id' AppUserId
+	}
+}
