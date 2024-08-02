5b2a7f9f63551de0ded76b176e55be2427eab9a4
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-02 18:37:39 +0300

    Update Identity tables in database

--- Repository/Identity/IdentityContext.cs
+++ Repository/Identity/IdentityContext.cs
@@ -1,5 +1,4 @@
 ﻿using Core.Entities.IdentityEntities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
--- Repository/Repository/Identity/20240802153655_Update-Identity-Tables.Designer.cs
+++ Repository/Repository/Identity/20240802153655_Update-Identity-Tables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository.Identity;
 
@@ -11,9 +12,11 @@ using Repository.Identity;
 namespace Repository.Repository.Identity
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20240802153655_Update-Identity-Tables")]
+    partial class UpdateIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
