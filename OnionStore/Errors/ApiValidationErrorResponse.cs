fc259face3d8e92605e12a1fc9e1790f4b19587d
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-02 18:52:05 +0300

    Handle validation error

---
+++ OnionStore/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Errors
+{
+	public class ApiValidationErrorResponse: ApiResponse
+	{
+		public ApiValidationErrorResponse() : base(400)
+		{
+
+		}
+
+		public IEnumerable<string> Errors { get; set; } = new List<string>();
+	}
+}
--- OnionStore/Program.cs
+++ OnionStore/Program.cs
@@ -1,4 +1,6 @@
+using API.Errors;
 using API.ServicesExtension;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repository.Identity;
 
@@ -15,6 +17,33 @@ builder.Services.AddSwaggerServices();
 // Add Identity Context and Configurations
 builder.Services.AddIdentityConfigurations(builder.Configuration);
 
+#endregion
+
+#region Validation Error - Bad Request
+// -- Validation Error (Bad Request) 
+// --- First: We need to bring options which have InvalidModelState
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+	// --- then we need all data (actionContext) of action has validation error
+	options.InvalidModelStateResponseFactory = (actionContext) =>
+	{
+		// --- then we bring ModelState: Dictionary key/value pair for each parameter, and value has property Errors Array have all errors
+		// --- and we use where to bring dictionary key/value pair which is value has errors 
+		var errors = actionContext.ModelState.Where(P => P.Value.Errors.Count() > 0)
+		// --- then we use SelectMany to make one array of all error  
+		.SelectMany(P => P.Value.Errors)
+		// --- then we use Select to bring from errors just ErrorMessages
+		.Select(E => E.ErrorMessage)
+		.ToArray();
+		// --- then we insert this errors to the class we made
+		var validationErrorResponse = new ApiValidationErrorResponse()
+		{
+			Errors = errors
+		};
+		// then return it :)
+		return new BadRequestObjectResult(validationErrorResponse);
+	};
+});
 #endregion
 
 var app = builder.Build();
