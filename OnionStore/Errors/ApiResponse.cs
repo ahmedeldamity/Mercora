300c203f1124f46e353829e071ba802b21bbc60c
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-02 18:48:40 +0300

    Handle not found, bad request and unauthorized errors

---
+++ OnionStore/Errors/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace API.Errors
+{
+	public class ApiResponse
+	{
+		public int StatusCode { get; set; }
+		public string? Message { get; set; }
+
+		public ApiResponse(int statusCode, string? message = null)
+		{
+			StatusCode = statusCode;
+			Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+		}
+
+		private string? GetDefaultMessageForStatusCode(int statusCode)
+		{
+			return statusCode switch
+			{
+				400 => "A bad request, you have made!",
+				401 => "Authorized, you are not!",
+				404 => "Resourse was not found!",
+				500 => "Server Error",
+				_ => null
+			};
+		}
+	}
+}
