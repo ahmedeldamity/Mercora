5b9eda9efe4263b8e09541bc63f2a6066424d60a
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-11 16:19:14 +0300

    Add product, Brand and Category entities

---
+++ Core/Entities/ProductEntities/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class ProductCategory : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+        // -- We Don't Bring Products From Product Category So We Don't Need Navigation Property
+        // -- But EF Consider This Relation ONE-ONE And We Need It ONE-MANY So We Modified It In Fluent API
+    }
+}
