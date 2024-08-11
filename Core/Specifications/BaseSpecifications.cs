77318286205fc84420962cd4c55c7163eb412efe
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-11 15:52:33 +0300

    Implement Specifications design pattern

---
+++ Core/Specifications/BaseSpecifications.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Interfaces.Specifications;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecifications<T> : ISpecifications<T> where T : BaseEntity
+    {
+        public Expression<Func<T, bool>> WhereCriteria { get; set; }
+        public List<Expression<Func<T, object>>> IncludesCriteria { get; set; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>> OrderBy { get; set; }
+        public Expression<Func<T, object>> OrderByDesc { get; set; }
+    }
+}
