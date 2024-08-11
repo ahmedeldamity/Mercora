7e16fa919a8bd33f820f6c00b827ccbdd567cd8e
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-11 16:30:19 +0300

    Add Product With Brands and Categories & Product Count Specifications

---
+++ Core/Specifications/ProductSpecifications/ProductSpecificationParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Specifications.ProductSpecifications
+{
+    public class ProductSpecificationParameters
+    {
+        public int? brandId { get; set; }
+        public int? categoryId { get; set; }
+        public string? sort { get; set; }
+        public string? search { get; set; }
+    }
+}
