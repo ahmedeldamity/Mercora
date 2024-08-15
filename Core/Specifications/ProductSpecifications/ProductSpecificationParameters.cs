5bf7c5bc30e6931f4f349342718de7f6baffe7ac
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-15 18:01:49 +0300

    Add Pagination Params to map it in Base Specifications

--- Core/Specifications/ProductSpecifications/ProductSpecificationParameters.cs
+++ Core/Specifications/ProductSpecifications/ProductSpecificationParameters.cs
@@ -2,6 +2,15 @@
 {
     public class ProductSpecificationParameters
     {
+        private const int MaxPageSize = 40;
+
+        private int pageSize = 10;
+        public int PageIndex { get; set; } = 1;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = value > MaxPageSize ? pageSize : value; }
+        }
         public int? brandId { get; set; }
         public int? categoryId { get; set; }
         public string? sort { get; set; }
