a3a0eb321162d78310231cfa35682b5ade92ec14
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-15 16:05:34 +0300

    Get Products endpoint

---
+++ OnionStore/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Entities.Product_Entities;
+using Core.Interfaces.Services;
+using Core.Specifications.ProductSpecifications;
+using DotNetCore_ECommerce.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProductController(IProductService _productService, IMapper _mapper) : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery] ProductSpecificationParameters specParams)
+        {
+            var products = await _productService.GetProductsAsync(specParams);
+
+            var productsDto = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(productsDto);
+        }
+    }
+}
