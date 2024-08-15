929cb13dbfc0ac6a80a765ea300654e46b974bf6
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-15 17:20:18 +0300

    Get Product end point

--- OnionStore/Controllers/ProductController.cs
+++ OnionStore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using API.Errors;
+using AutoMapper;
 using Core.Entities.Product_Entities;
 using Core.Interfaces.Services;
 using Core.Specifications.ProductSpecifications;
@@ -17,6 +18,19 @@ namespace API.Controllers
             var productsDto = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
             return Ok(productsDto);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ProductToReturnDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProduct(int id)
+        {
+            var product = await _productService.GetProductAsync(id);
+
+            if (product is null)
+                return NotFound(new ApiResponse(404));
+
+            return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
         }
     }
 }
