3b83c10547b68d5a65a200bfcb6dbd505caf4065
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-21 22:43:58 +0300

    Implement basket repository and add basket crud in it

---
+++ Core/Interfaces/Repositories/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entities.BasketEntities;
+
+namespace Core.Interfaces.Repositories;
+public interface IBasketRepository
+{
+    Task<Basket?> CreateOrUpdateBasketAsync(Basket basket);
+    Task<Basket?> GetBasketAsync(string basketId);
+    Task<bool> DeleteBasketAsync(string basketId);
+}
---
+++ Repository/BasketRepository.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities.BasketEntities;
+using Core.Interfaces.Repositories;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Repository;
+public class BasketRepository: IBasketRepository
+{
+    private readonly IDatabase _database;
+    public BasketRepository(IConnectionMultiplexer connection)
+    {
+        _database = connection.GetDatabase();
+    }
+    public async Task<Basket?> CreateOrUpdateBasketAsync(Basket basket)
+    {
+        var createdOrUpdated = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+        if (createdOrUpdated is false) return null;
+        return await GetBasketAsync(basket.Id);
+    }
+    public async Task<bool> DeleteBasketAsync(string basketId)
+    {
+        return await _database.KeyDeleteAsync(basketId);
+    }
+    public async Task<Basket?> GetBasketAsync(string basketId)
+    {
+        var basket = await _database.StringGetAsync(basketId);
+        return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<Basket>(basket!);
+    }
+}
