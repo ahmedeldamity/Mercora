f5a3116fe0535abad731d27a973c4b6a714ae576
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-24 15:47:31 +0300

    Product order item, order item configurations

---
+++ Repository/Store/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.OrderEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Store.Configurations;
+public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.OwnsOne(orderItem => orderItem.Product, Product => Product.WithOwner());
+
+        builder.Property(p => p.Price)
+            .HasColumnType("decimal(6,2)");
+
+        builder.Property(p => p.Quantity)
+            .HasColumnType("decimal(8,2)");
+    }
+}
---
+++ Repository/Store/Configurations/ProductOrderItemConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.OrderEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Store.Configurations;
+public class ProductOrderItemConfigurations: IEntityTypeConfiguration<ProductOrderItem>
+{
+    public void Configure(EntityTypeBuilder<ProductOrderItem> builder)
+    {
+        builder.Property(p => p.ProductName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.ProductImageCover)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
