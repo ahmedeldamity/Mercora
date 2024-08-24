9446185bfeaef285a1fa9a1855ffe232daa1d011
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-24 15:36:24 +0300

    Order entities

---
+++ Core/Entities/OrderEntities/OrderDeliveryMethod.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Entities.OrderEntities;
+public class OrderDeliveryMethod: BaseEntity
+{
+    public OrderDeliveryMethod() { /* we create this constractor because EF need it while migration to make instance from this class */ }
+    public OrderDeliveryMethod(string name, string description, decimal cost, string deliveryTime)
+    {
+        Name = name;
+        Description = description;
+        Cost = cost;
+        DeliveryTime = deliveryTime;
+    }
+
+    public string Name { get; set; } = null!; // the name of delivery way
+    public string Description { get; set; } = null!;
+    public decimal Cost { get; set; }
+    public string DeliveryTime { get; set; } = null!; // the time delivery will take to bring the order
+}
---
+++ Core/Entities/OrderEntities/OrderStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Entities.OrderEntities;
+public enum OrderStatus
+{
+    pending,
+    paymentSucceeded,
+    paymentFailed
+}
