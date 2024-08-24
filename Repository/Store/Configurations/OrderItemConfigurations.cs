6669f10f67d82e477af65f1cee50502aa793254a
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-24 16:16:48 +0300

    Mapped order entities in database

--- Repository/Store/Configurations/OrderItemConfigurations.cs
+++ Repository/Store/Configurations/OrderItemConfigurations.cs
@@ -7,12 +7,23 @@ public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
 {
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
-        builder.OwnsOne(orderItem => orderItem.Product, Product => Product.WithOwner());
+        builder.OwnsOne(orderItem => orderItem.Product, Product =>
+        {
+            Product.WithOwner();
+
+            Product.Property(p => p.ProductName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+            Product.Property(p => p.ProductImageCover)
+                .IsRequired()
+                .HasMaxLength(100);
+        });
 
         builder.Property(p => p.Price)
             .HasColumnType("decimal(6,2)");
 
         builder.Property(p => p.Quantity)
-            .HasColumnType("decimal(8,2)");
+            .HasColumnType("decimal(9,2)");
     }
 }
--- Repository/Store/Migrations/20240824131409_OrderTables.Designer.cs
+++ Repository/Store/Migrations/20240824131409_OrderTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository.Store;
 
@@ -11,9 +12,11 @@ using Repository.Store;
 namespace Repository.Store.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240824131409_OrderTables")]
+    partial class OrderTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
