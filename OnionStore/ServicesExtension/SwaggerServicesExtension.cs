7a2826ebb6947614836161eed964dc4dafd680f5
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-25 03:56:39 +0300

    Add Authentication to Swagger Gen

--- OnionStore/ServicesExtension/SwaggerServicesExtension.cs
+++ OnionStore/ServicesExtension/SwaggerServicesExtension.cs
@@ -1,13 +1,40 @@
-﻿namespace API.ServicesExtension;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace API.ServicesExtension;
 public static class SwaggerServicesExtension
 {
 	public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
 	{
 		services.AddEndpointsApiExplorer();
 
-		services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer token\"",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = JwtBearerDefaults.AuthenticationScheme,
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+        });
 
-		return services;
+        return services;
 	}
 
 	public static WebApplication UseSwaggerMiddleware(this WebApplication app)
