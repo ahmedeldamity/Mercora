cd26f2253f47d1ed5f3e7246f8f6a98264c9a951
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-27 17:54:01 +0300

    Remove un used basket properties

--- Core/Entities/BasketEntities/Basket.cs
+++ Core/Entities/BasketEntities/Basket.cs
@@ -1,15 +1,10 @@
 ﻿namespace Core.Entities.BasketEntities;
-public class Basket
+public class Basket(string id)
 {
-    public string Id { get; set; }
+    public string Id { get; set; } = id;
     public List<BasketItem> Items { get; set; } = [];
     public int? DeliveryMethodId { get; set; }
     public decimal? ShippingPrice { get; set; }
     public string? PaymentIntentId { get; set; }
     public string? ClientSecret { get; set; }
-    public Basket(string id)
-    {
-        Id = id;
-        Items = new List<BasketItem>();
-    }
 }
--- Shared/Dtos/BasketRequest.cs
+++ Shared/Dtos/BasketRequest.cs
@@ -3,8 +3,5 @@ public class BasketRequest
 {
     public string Id { get; set; } = null!;
     public List<BasketItemRequest> Items { get; set; } = [];
-    public string? PaymentIntentId { get; set; }
-    public string? ClientSecret { get; set; }
     public int? DeliveryMethodId { get; set; }
-    public decimal? ShippingPrice { get; set; }
 }
