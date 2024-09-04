71e1cf8897d4f40a3807313cce8c34383a9feee7
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-09-04 22:34:47 +0300

    Add product services and endpoints

--- Core/Interfaces/Services/IProductService.cs
+++ Core/Interfaces/Services/IProductService.cs
@@ -7,4 +7,7 @@ public interface IProductService
 {
     Task<Result<PaginationToReturn<ProductResponse>>> GetProductsAsync(ProductSpecificationParameters specParams);
     Task<Result<ProductResponse>> GetProductAsync(int id);
+    Task<Result<ProductResponse>> CreateProductAsync(ProductRequest productRequest);
+    Task<Result<ProductResponse>> UpdateProductAsync(int id, ProductRequest productRequest);
+    Task<Result<ProductResponse>> DeleteProductAsync(int id);
 }
