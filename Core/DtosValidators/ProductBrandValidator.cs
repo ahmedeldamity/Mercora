fa0770499dff69b99191442881e6cdc50b8b6377
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-09-06 15:11:03 +0300

    Add product, brand, category and token API validations

---
+++ Core/DtosValidators/ProductBrandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Core.Dtos;
+using FluentValidation;
+
+namespace Core.DtosValidators;
+
+public class ProductBrandValidator: AbstractValidator<ProductBrandRequest>
+{
+    public ProductBrandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required")
+            .MaximumLength(100)
+            .WithMessage("Name must not exceed 100 characters");
+
+        RuleFor(x => x.ImageCover)
+            .NotEmpty()
+            .WithMessage("Image cover is required")
+            .MaximumLength(100)
+            .WithMessage("Image cover must not exceed 100 characters");
+    }
+
+}
