0d05dd936bb9cd8d10ee97b6acda25589bf2de18
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-10-12 20:13:49 +0300

    Add Category Service In Client Project

---
+++ src/Presentation/BlazorEcommerce.Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorEcommerce.Shared.Category;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services.CategoryService;
+public class CategoryService(HttpClient httpClient): ICategoryService
+{
+	public async Task<List<CategoryResponse>> GetAllCategories()
+	{
+		var response = await httpClient.GetFromJsonAsync<List<CategoryResponse>>("api/Category");
+
+		return response ?? [];
+	}
+}
---
+++ src/Presentation/BlazorEcommerce.Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,7 @@
+﻿using BlazorEcommerce.Shared.Category;
+
+namespace BlazorEcommerce.Client.Services.CategoryService;
+public interface ICategoryService
+{
+	Task<List<CategoryResponse>> GetAllCategories();
+}
