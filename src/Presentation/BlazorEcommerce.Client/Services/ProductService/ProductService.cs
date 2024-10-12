2c3b3ff45d1f9595314cb3c3e8985b382a618522
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-10-12 17:59:00 +0300

    Enable Pagination In Product Service In The Client Project

--- src/Presentation/BlazorEcommerce.Client/Services/ProductService/ProductService.cs
+++ src/Presentation/BlazorEcommerce.Client/Services/ProductService/ProductService.cs
@@ -7,7 +7,21 @@ public class ProductService(HttpClient httpClient) : IProductService
 {
 	public async Task<PaginationToReturn<ProductResponse>?> GetProductsAsync(ProductParameters specParams)
 	{
-		var response = await httpClient.GetFromJsonAsync<PaginationToReturn<ProductResponse>>("api/Product");
+		var uri = "api/Product?pageIndex=" + specParams.PageIndex;
+
+		if (specParams.BrandId.HasValue)
+			uri += "&brandId=" + specParams.BrandId;
+
+		if (specParams.CategoryId.HasValue)
+			uri += "&categoryId=" + specParams.CategoryId;
+
+		if (!string.IsNullOrWhiteSpace(specParams.Sort))
+			uri += "&sort=" + specParams.Sort;
+
+		if (!string.IsNullOrWhiteSpace(specParams.Search))
+			uri += "&search=" + specParams.Search;
+
+		var response = await httpClient.GetFromJsonAsync<PaginationToReturn<ProductResponse>>(uri);
 
 		return response;
 	}
