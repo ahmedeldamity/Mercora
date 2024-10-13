99a7c142c77d3036a213e9824b67f95becac2c0a
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-10-13 21:43:47 +0300

    Modify Product Count Specification For Search on Description Also

--- src/Core/BlazorEcommerce.Application/Specifications/ProductSpecifications/ProductCountSpecification.cs
+++ src/Core/BlazorEcommerce.Application/Specifications/ProductSpecifications/ProductCountSpecification.cs
@@ -5,9 +5,11 @@ public class ProductCountSpecification : BaseSpecifications<Product>
 {
     public ProductCountSpecification(ProductSpecificationParameters specParams)
     {
-        WhereCriteria =
-           p => (string.IsNullOrEmpty(specParams.Search) || p.Name.ToLower().Contains(specParams.Search.ToLower())) &&
-                (!specParams.BrandId.HasValue || p.BrandId == specParams.BrandId.Value) &&
-                (!specParams.CategoryId.HasValue || p.CategoryId == specParams.CategoryId.Value);
-    }
+        WhereCriteria = p =>
+	        (string.IsNullOrEmpty(specParams.Search) ||
+	         p.Name.ToLower().Contains(specParams.Search.ToLower()) ||
+	         p.Description.ToLower().Contains(specParams.Search.ToLower())) &&
+	        (!specParams.BrandId.HasValue || p.BrandId == specParams.BrandId.Value) &&
+	        (!specParams.CategoryId.HasValue || p.CategoryId == specParams.CategoryId.Value);
+	}
 }
