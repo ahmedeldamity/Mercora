a43306a4e829f996f524f3d6ebf845144cf0c160
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-10-16 13:20:06 +0300

    Add Order Totals Page

--- src/Presentation/BlazorEcommerce.Client/Program.cs
+++ src/Presentation/BlazorEcommerce.Client/Program.cs
@@ -1,6 +1,9 @@
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.CartService;
 using BlazorEcommerce.Client.Services.CategoryService;
 using BlazorEcommerce.Client.Services.ProductService;
+using Blazored.LocalStorage;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -15,8 +18,14 @@ builder.Services.AddScoped(sp => new HttpClient
 	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped(typeof(IProductService), typeof(ProductService));
 
 builder.Services.AddScoped(typeof(ICategoryService), typeof(CategoryService));
+
+builder.Services.AddScoped(typeof(ICartService), typeof(CartService));
 
 await builder.Build().RunAsync();
